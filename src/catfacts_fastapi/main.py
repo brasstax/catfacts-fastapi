c1c6542c6f72b18a149de82fb1809eca8420210c
Brass Tax <brasstax@users.noreply.github.com>
2022-06-11 22:41:54 -0700

    refactor to use sqlalchemy to create and manage database.

--- src/catfacts_fastapi/main.py
+++ src/catfacts_fastapi/main.py
@@ -1,10 +1,9 @@
-from typing import Union
+from typing import Union, Dict
 import logging
 from os.path import exists
 import sys
 
 from fastapi import FastAPI
-from catfacts_fastapi import utils
 from databases import Database
 
 logging.basicConfig(level=logging.INFO)
@@ -15,6 +14,9 @@ app = FastAPI()
 v2 = FastAPI()
 
 
+async def read_random() -> Dict:
+  return {"Foo": "Bar"}
+
 @app.on_event("startup")
 async def startup_event():
     if not exists(database_filename):
@@ -23,20 +25,27 @@ async def startup_event():
         sys.exit(1)
     await database.connect()
 
+@app.on_event("shutdown")
+async def database_disconnect():
+    await database.disconnect()
 
 @app.get("/")
 async def read_root():
-    return utils.read_random()
+    results = await read_random()
+    return results
 
 
 @v2.get("/")
 async def read_random_v2_root():
-    return utils.read_random()
+    query = "SELECT * FROM catfacts"
+    results = await database.fetch_all(query=query)
+    return results
 
 
 @v2.get("/random")
 async def read_random_v2_endpoint():
-    return utils.read_random()
+    results = await read_random()
+    return results
 
 
 app.mount("/v2", v2)
--- src/catfacts_fastapi/utils.py
+++ src/catfacts_fastapi/utils.py
@@ -1,28 +1,40 @@
 import importlib.resources as pkg_resources
+import asyncio
+from typing import Dict
 import catfacts_fastapi
-import pdb
 import sqlite3
+import sqlalchemy
+from databases import Database
 
-def init_catfacts():
+async def init_catfacts_async():
   with pkg_resources.open_text(catfacts_fastapi, "catfacts.txt") as f:
     catfacts_txt = f.read().splitlines()
   
+  metadata = sqlalchemy.MetaData()
+  catfacts = sqlalchemy.Table(
+    "catfacts",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("fact", sqlalchemy.Text),
+  )
+
   con = sqlite3.connect("catfacts.db")
   cur = con.cursor()
   cur.execute("DROP table IF EXISTS catfacts")
   con.commit()
-  cur.execute(
-    """
-    CREATE TABLE catfacts(id INTEGER PRIMARY KEY AUTOINCREMENT, fact TEXT)
-    """
-  )
-  con.commit()
-
-  for fact in catfacts_txt:
-    cur.execute("INSERT INTO catfacts(fact) VALUES (?)", (fact,))
-  con.commit()
   con.close()
 
+  engine = sqlalchemy.create_engine("sqlite:///catfacts.db", echo=True)
+  metadata.create_all(engine)
 
-def read_random():
-  return {"Foo": "Bar"}
+  database = Database("sqlite:///catfacts.db")
+
+  query = catfacts.insert()
+  for fact in catfacts_txt:
+    values = {"fact": fact}
+    await database.execute(query=query, values=values)
+  
+  await database.disconnect()
+
+def init_catfacts():
+  asyncio.run(init_catfacts_async())
